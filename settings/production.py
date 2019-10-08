ba7e81002f7a4e977d8328309d1f81b4d36b19ec
christwin <ichristwin@gmail.com>
2019-10-08 14:18:08 +0100

    whitenoise in again, this time for media files

--- settings/production.py
+++ settings/production.py
@@ -9,6 +9,19 @@ from .base import *
 
 DATABASES['default'] = dj_database_url.config()
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    # I added whitenoise here
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -20,5 +33,11 @@ ALLOWED_HOSTS = ['the-great-bazaar.herokuapp.com',
 
 # Set debug to False
 DEBUG = False
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 django_heroku.settings(locals())
