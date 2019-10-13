b0ac0146d922d90f5d4a4eaeaab3f5533769574d
christwin <ichristwin@gmail.com>
2019-10-13 20:57:32 +0100

    debug

--- settings/production.py
+++ settings/production.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ['the-great-bazaar.herokuapp.com',
                  ]
 
 # Set debug to False
-DEBUG = False
+DEBUG = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
